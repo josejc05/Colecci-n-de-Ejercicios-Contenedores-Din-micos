d8f4824a2bd608ff5270ed849515b3e56167f429
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-30 11:05:20 +0100

    Ej 2 semicompleto

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -4,46 +4,12 @@
 
 using namespace std;
 
-void mostrarLista(const list<double>& l) {
-    cout << "Elementos de la lista: ";
-    for (const double& elemento : l) {
-        cout << elemento << " ";
-    }
-    cout << endl;
-}
+int main() {
+    list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
+    stack<float, list<float>> pila(lista);
 
-void mostrarPila(stack<double, list<double>>& pila) {
-    cout << "Elementos de la pila: ";
+    cout << "Contenido inicial de la pila:" << endl;
     while (!pila.empty()) {
-        cout << pila.top() << " ";
+        cout << pila.top() << endl;
         pila.pop();
     }
-    cout << endl;
-}
-int main() {
-    list<double> miLista = {1.1, 2.2, 3.3, 4.4};
-
-    stack<double, list<double>> miPila(miLista);
-
-    cout << "Operaciones en la lista y la pila:" << endl;
-
-    mostrarLista(miLista);
-
-    miPila.push(5.5);
-    cout << "Después de push(5.5), top: " << miPila.top() << endl;
-
-    mostrarLista(miLista);
-
-    miPila.pop();
-    cout << "Después de pop(), top: " << miPila.top() << endl;
-
-    mostrarLista(miLista);
-    miPila.push(6.6);
-    cout << "Después de push(6.6), top: " << miPila.top() << endl;
-
-    mostrarLista(miLista);
-
-    mostrarPila(miPila);
-
-    return 0;
-}
