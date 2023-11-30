56d5a6ba99445f9812c67c1e03e5ffb920e76b32
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-30 11:02:31 +0100

    Ej 1 semicompleto

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -3,43 +3,9 @@
 
 using namespace std;
 
-void mostrarVector(const vector<int>& v) {
-    cout << "Elementos del vector: ";
-    for (const int& elemento : v) {
-        cout << elemento << " ";
-    }
-    cout << endl;
-}
-int calcularSuma(const vector<int>& v) {
-    int suma = 0;
-    for (const int& elemento : v) {
-        suma += elemento;
-    }
-    return suma;
-}
-
-int encontrarMaximo(const vector<int>& v) {
-    int maximo = v[0];
-    for (const int& elemento : v) {
-        if (elemento > maximo) {
-            maximo = elemento;
-        }
-    }
-    return maximo;
-}
 int main() {
-    vector<int> miVector;
+    vector<int> numeros = {10, 20, 30, 40, 50};
 
-    miVector.push_back(10);
-    miVector.push_back(20);
-    miVector.push_back(30);
-    miVector.push_back(40);
-
-    mostrarVector(miVector);
-
-    cout << "Suma de los elementos: " << calcularSuma(miVector) << endl;
-
-    cout << "Valor máximo en el vector: " << encontrarMaximo(miVector) << endl;
-
-    return 0;
-}
+    numeros.push_back(60);
+    numeros.push_back(70);
+    numeros.push_back(80);
